0f8efaac4ffd08b12ee62744a2ad9c14047dd91c
krangelie <kraftangelie@gmail.com>
2021-11-26 15:51:16 +0100

    adjust bias eval GER & evaluate GPT-3 new GER generations

--- src/classifier/torch_helpers/load_pretrained.py
+++ src/classifier/torch_helpers/load_pretrained.py
@@ -1,3 +1,5 @@
+import os
+
 import hydra
 import torch
 from transformers import BertForSequenceClassification, BertTokenizer
@@ -9,16 +11,19 @@ def load_torch_model(model_path, model_type, logger=None):
     model_path = hydra.utils.to_absolute_path(model_path)
     if logger is not None:
         logger.info(f"Loading pretrained torch model from {model_path}")
-    if "bert_regard_v2_large" in model_path:
-        print(model_path)
+    if "EN" in model_path:
         tokenizer = BertTokenizer.from_pretrained(model_path)
         model = BertForSequenceClassification.from_pretrained(model_path)
         return (model, tokenizer)
-    elif model_path.endswith("pth"):
-        model = torch.load(model_path, map_location=torch.device('cpu'))
-    elif model_path.endswith("ckpt"):
-        if model_type == "lstm":
-            model = RegardLSTM.load_from_checkpoint(model_path)
-        elif model_type == "transformer":
-            model = RegardBERT.load_from_checkpoint(model_path)
+    else:
+        model_file = os.listdir(model_path)[0]
+        print("Models found in directory", model_file)
+        model_path = os.path.join(model_path, model_file)
+        if model_path.endswith("pth"):
+            model = torch.load(model_path, map_location=torch.device('cpu'))
+        elif model_path.endswith("ckpt"):
+            if model_type == "lstm":
+                model = RegardLSTM.load_from_checkpoint(model_path)
+            elif model_type == "transformer":
+                model = RegardBERT.load_from_checkpoint(model_path)
     return model
