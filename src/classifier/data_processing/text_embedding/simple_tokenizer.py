373ceb022f723aa7f3a023b25b538b4b0c75446d
AdaAngelie <angelie.kraft@adalab.ai>
2021-11-30 10:59:39 +0100

    remove lemmatizing and stemming options and make simple tokenizer language independent

--- src/classifier/data_processing/text_embedding/simple_tokenizer.py
+++ src/classifier/data_processing/text_embedding/simple_tokenizer.py
@@ -5,24 +5,19 @@ from pandas import DataFrame
 import nltk
 
 
-class SimpleGermanTokenizer:
+class SimpleTokenizer:
     """Simple tokenization for sentences. Used for non-Transformer-based models."""
 
     def __init__(
         self,
+        language,
         to_lower=True,
         use_remove_punctuation=True,
-        use_lemmatize=False,
-        use_stem=False,
     ):
+        self.language = language
         self.to_lower = to_lower
         self.use_remove_punctuation = use_remove_punctuation
-        self.use_lemmatize = use_lemmatize
-        self.use_stem = use_stem
-        self.lemmatizer = (
-            ht.HanoverTagger("morphmodel_ger.pgz") if use_lemmatize else None
-        )
-        self.stemmer = Cistem() if use_stem else None
+
 
     def tokenize(self, df: DataFrame, text_col="text") -> DataFrame:
         """
@@ -39,15 +34,8 @@ class SimpleGermanTokenizer:
 
         # Split sentences into list of words
         df["tokenized"] = texts.apply(
-            lambda text: nltk.tokenize.word_tokenize(text, language="german")
+            lambda text: nltk.tokenize.word_tokenize(text, language=self.language)
         )
-
-        if self.use_lemmatize:
-            df["lemmata"] = df["tokenized"].apply(lambda tokens: self.lemmatize(tokens))
-
-        if self.use_stem:
-            df["stems"] = df["tokenized"].apply(lambda tokens: self.stem(tokens))
-
         return df
 
     def remove_punctuation(self, text):
@@ -56,16 +44,3 @@ class SimpleGermanTokenizer:
             if punc in text:
                 text = text.replace(punc, " ")
         return text.strip()
-
-    # https://textmining.wp.hs-hannover.de/Preprocessing.html
-    def lemmatize(self, tokenized_wordlist):
-        lemmata = self.lemmatizer.tag_sent(
-            tokenized_wordlist, taglevel=1
-        )  # What is tag level? # return 3-tuple (word, lemma, POS-tag)
-        # pprint(lemmata)
-        lemmata_only = [entry[1] for entry in lemmata]
-        return lemmata_only
-
-    def stem(self, tokenized_wordlist):
-        stems = [self.stemmer.stem(token) for token in tokenized_wordlist]
-        return stems
